4bbb83923ae44d2bcb468c70cff9d59dbb59b5fc
Ryan Lence <data@lence.ai>
2023-08-05 09:06:26 -0700

    mm_1

--- code/05-interactive-code/m-n-m/wrk.py
+++ code/05-interactive-code/m-n-m/wrk.py
@@ -5,4 +5,22 @@ print('M&M Guessing gamel')
 
 mm_count = random.randint(1, 100)
 
-print(mm_count)
+print('Guess the numver')
+attempt_limit = 7
+attempts = 0
+
+while attempts < attempt_limit:
+    guess_text = input('How many MM are their in the jar ')
+    guess = int(guess_text)
+
+    if mm_count == guess:
+        print(f'You get free lunch {guess}.')
+        break
+    elif guess < mm_count:
+        print('Sorry, to low')
+    else:
+        print('that is to high')
+
+    attempts += 1
+
+print('Bye')
